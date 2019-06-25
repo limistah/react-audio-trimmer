1ce0cbb1be24a52da3cdc78d9b335e9ce4022e1f
Aleem Isiaka <aleemisiaka@gmail.com>
2019-06-25 21:21:31 +0100

    web audio

--- src/index.js
+++ src/index.js
@@ -2,37 +2,50 @@ import "@mohayonao/web-audio-api-shim";
 import React, { Component } from "react";
 import "./assets/styles";
 import FilePicker from "./components/FilePicker";
+import { isAudio, readBlobURL, download, rename } from "./libs/utils";
+import WebAudio from "./libs/webAudio";
 
 export default class ReactAudioTrimmer extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      file: null,
+      decoding: false,
+      audioBuffer: null,
+      paused: true,
+      startTime: 0,
+      endTime: Infinity,
+      currentTime: 0,
+      processing: false
+    };
   }
 
   handleFileChange = async file => {
     console.log(file);
-    // if (!isAudio(file)) {
-    //   return alert('请选择合法的音频文件')
-    // }
+    if (!isAudio(file)) {
+      return alert("Unsupported File Type");
+    }
 
-    // this.setState({
-    //   file,
-    //   paused: true,
-    //   decoding: true,
-    //   audioBuffer: null,
-    // })
+    this.setState({
+      file,
+      paused: true,
+      decoding: true,
+      audioBuffer: null
+    });
 
-    // const audioBuffer = await WebAudio.decode(file)
+    const audioBuffer = await WebAudio.decode(file);
 
-    // window.audioBuffer = audioBuffer
+    window.audioBuffer = audioBuffer;
 
-    // this.setState({
-    //   paused: false,
-    //   decoding: false,
-    //   audioBuffer,
-    //   startTime: 0,
-    //   currentTime: 0,
-    //   endTime: audioBuffer.duration / 2,
-    // })
+    this.setState({
+      paused: false,
+      decoding: false,
+      audioBuffer,
+      startTime: 0,
+      currentTime: 0,
+      endTime: audioBuffer.duration / 2
+    });
   };
 
   render() {
