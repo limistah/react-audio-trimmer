8685946c5e871d10eda27a073e78afee1de07557
Aleem Isiaka <aleemisiaka@gmail.com>
2019-06-25 18:33:14 +0100

    More structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "@mohayonao/web-audio-api-shim";
+import React, { PureComponent } from "react";
+import ReactDOM from "react-dom";
+import Lib from "./index.js";
+
+class Main extends PureComponent {
+  render() {
+    return (
+      <div className="page-container">
+        <Lib />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("main"));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "@mohayonao/web-audio-api-shim";
+
+export default class ReactAudioTrimmer {
+  render() {
+    return <div>Our Lovely Audio Trimmer</div>;
+  }
+}
